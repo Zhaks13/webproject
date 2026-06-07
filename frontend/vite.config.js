96d320904fcab2f2c25eb958c6d3d2fa095078e2
Zhaks <zhaksprogan@gmail.com>
2026-06-07 16:09:16 +0500

    Fix vite config import

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-    plugins: [react()], // или что там у тебя
+    plugins: [react()],
     server: {
         host: '0.0.0.0',
         port: 3000,
